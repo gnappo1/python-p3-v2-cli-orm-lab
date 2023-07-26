69a794c6695d576a2245786e60a79edea69ab999
Matteo Piccini <gnappo.1@live.it>
2023-07-26 07:17:56 -0700

    Fix missing colon on line 61

--- lib/helpers.py
+++ lib/helpers.py
@@ -58,7 +58,7 @@ def update_department():
 
 def delete_department():
     id_ = input("Enter the department's id: ")
-    if department := Department.find_by_id(id_)
+    if department := Department.find_by_id(id_):
         department.delete()
         print(f'Department {id_} deleted')
     else:
